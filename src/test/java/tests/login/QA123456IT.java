2a49553127c9e7c75c7f8f6542a16876408c8020
matheuswachtler <matheus.wachtler@outlook.com>
2025-07-02 12:26:33 -0300

    update test class

--- src/test/java/tests/login/QA123456IT.java
+++ src/test/java/tests/login/QA123456IT.java
@@ -13,10 +13,9 @@ public class QA123456IT extends HooksManager {
     @Test
     @DisplayName("QA123456 - Validates access to the application with valid credentials")
     @Tag("login")
-    @Tag("regression")
     @Description("As a Sauce Labs user," +
             "I want to access the system with my valid credentials,\n" +
-            "so that I can use the functionalities available after logging in.")
+          h  "so that I can use the functionalities available after logging in.")
     public void QA123456() throws Exception {
         LoginFunc login = new LoginFunc(DriverManager.getDriver());
 
